5f4031783360b154d95c8f187c7f807897f5f108
Mark Skriblovskiy <markus20022002@mail.ru>
2023-04-16 10:41:28 +0300

    Написал тесты для функции add new book

--- tests.py
+++ tests.py
@@ -20,5 +20,20 @@ class TestBooksCollector:
         # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
         assert len(collector.get_books_rating()) == 2
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    # Нельзя добавить одну и ту же книгу дважды
+    def test_add_new_book_add_similar_book(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        collector.add_new_book('Гордость и предубеждение и зомби')
+
+        assert collector.get_book_rating('Гордость и предубеждение и зомби') == 1
+
+    # Проверяем некорректные значения при добавлении книги
+    @pytest.mark.parametrize("new_book", ['', 12345, None])
+    def test_add_new_book_add_empty_or_incorrect_shows_error(self, new_book):
+        collector = BooksCollector()
+
+        collector.add_new_book(new_book)
+        
+        assert collector.get_book_rating(new_book) != 1
+        
